88460fcc4c6cb250151f7b3d1c1684f4f2348465
Dora3103 <80643439+Dora3103@users.noreply.github.com>
2021-07-13 03:35:49 +0200

    logout poslala na pocetna.php???

---
+++ main-page/logout.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+
+/*echo 'Are you sure you want to logout? . <br><br>';*/
+
+//echo '<a href="stranica.php">Return to the main page</a> . <br><br>';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="logout.css">
+    <meta charset="UTF-8">
+    <title>Logout</title>
+    <style>
+</style>
+</head>
+<body>
+
+<div id = "logo">
+    <h2>Are you sure you want to logout?</h2><br><br>
+    <div>
+    <a href="main2.html" class="button1"><h3>Return to the main page</h3></a>
+    <form method="post" action="../view/pocetna.php">
+			<button type="submit" id = "button2" name="logout" value="yes"><h3>Logout</h3></button>
+	</form>
+    </div>
+</div>
+
+</body>
+</html>
