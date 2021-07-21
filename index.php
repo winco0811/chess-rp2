144da46ba570252357437bb333693baed323741d
Vinko Lovrenčić <winco@acer.home.com>
2021-07-21 08:17:49 +0200

    stavio redirect u index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-Uspjesan login
 <?php
-require_once 'app/database/db.class.php';
-require_once "model/game.class.php";
-
-session_start();
-echo $_SESSION["color"];
-echo $_SESSION["username"];
-echo $_SESSION["gameId"];
-
- ?>
+header('Location: view/chess.php');
+?>
